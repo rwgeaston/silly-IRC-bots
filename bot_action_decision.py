0be65e3502e9fadc6ffc6bbdf3fa8dfd3e723a79
Robert Easton <rwgeaston@gmail.com>
2013-03-22 14:00:20 +0000

    forgot to send blank message list

--- bot_action_decision.py
+++ bot_action_decision.py
@@ -2,13 +2,12 @@ from bots import botmap_config
 reload(botmap_config)
 
 def actions(bot, source, text, private):
+    messages = []
     decision_module = botmap_config.bot_map[bot.nickname]
     if bot.nickname in text and ("shup" in text or ('shut' in text and 'up' in text)):
         if decision_module.authorised_to_shup(source):
             bot.messenger.messenger.wipe()
-            bot.messenger.messenger.add_to_queue(
-                                bot.channel,
-                                ["ok... :("])
+            messages = ["ok... :("]
     else:
         messages = decision_module.what_to_say(source,
                                                text,
