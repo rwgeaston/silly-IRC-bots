5efcd867d60592d24615434279ec459fef9bae9d
rwgeaston <rwgeaston@gmail.com>
2013-08-26 19:08:41 +0100

    battleships finished but not tested

--- bots/battleships_speech.py
+++ bots/battleships_speech.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 def authorised_to_shup(source, owner):
     return source == owner
 
@@ -60,47 +63,51 @@ class BattleshipsBot(object):
         return ['{source} has challenged {target}! Waiting for accept...'
                 .format(source=source, target=target)]
 
-    def cancel(self, source, nickname):
-        for index, game in enumerate(self.active_games):
-            if source in game.players:
-                if game.status in ['challenge', 'waiting for positions']:
-                    del self.active_games[index]
-                    return ["{} has cancelled a game."
-                            .format(source)]
-                else:
-                    return ["{}: Too late to cancel that game. See it through"]
-        return ["{source}: uh, I can't find anything to cancel"
-                .format(source=source)]
+    def cancel(self, source):
+        index, game = self.find_my_game(source)
+        if not game:
+            return ["{source}: uh, I can't find anything to cancel"
+                    .format(source=source)]
 
-    def forfeit(self, source, nickname):
-        for index, game in enumerate(self.active_games):
-            if source in game.players:
-                if game.status in ['playing']:
-                    players = game.players
-                    for player in players:
-                        if player != source:
-                            forfeit_beneficiary = player
-                            break
-                    del self.active_games[index]
-                    return ["{} has forfeited a game."
-                            .format(source),
-                            "{} wins!".format(forfeit_beneficiary)]
-                else:
-                    return self.cancel(source, nickname)
-        return ["{source}: uh, I can't find anything to forfeit"
-                .format(source=source)]
+        if game.status in ['challenge', 'waiting for positions']:
+            del self.active_games[index]
+            return ["{} has cancelled a game."
+                    .format(source)]
+        else:
+            return ["{}: Too late to cancel that game. See it through or forfeit."]
 
-    def accept(self, source, nickname):
-        for game in self.active_games:
-            if source in game.players:
-                if game.status == 'challenge':
-                    game.status = 'waiting for positions'
-                    return ["{} has accepted! Please PM me your boat positions."
-                            .format(source)]
-                else:
-                    return ["{}: You're already playing"]
-        return ["{source}: I'm sorry to have to be the one to tell you this, but no one has challenged you yet."
-                .format(source=source)]
+    def find_my_game(self, player_name):
+        for index, game in enumerate(self.active_games):
+            if player_name in game.players:
+                return index, game
+        return False, False
+
+    def forfeit(self, source):
+        index, game = self.find_my_game(source)
+        if not game:
+            return ["{source}: uh, I can't find anything to forfeit"
+                    .format(source=source)]
+        if game.status in ['playing']:
+            forfeit_beneficiary = game.players[(game.players.index(source) + 1) % 2]
+            del self.active_games[index]
+            return ["{} has forfeited a game."
+                    .format(source),
+                    "{} wins!".format(forfeit_beneficiary)]
+        else:
+            return self.cancel(source)
+
+    def accept(self, source):
+        _, game = self.find_my_game(source)
+        if not game:
+            return ["{source}: I'm sorry to have to be the one to tell you this, but no one has challenged you yet."
+                    .format(source=source)]
+        if game.status == 'challenge':
+            game.status = 'waiting for positions'
+            return ["{} has accepted! Please PM me your boat positions."
+                    .format(source)]
+        else:
+            return ["{}: You're already playing"]
+
 
     def current_games(self, source, nickname):
         return [game.info() for game in self.active_games]
@@ -187,6 +194,8 @@ class BattleshipsGame(object):
             ['carrier', 'battleship', 'submarine', 'destroyer', 'patrol']
             for _ in range(2)
         ]
+        self.moves = [set(), set()]
+        self.boats_left_to_sink = [5, 5]
 
     def info(self):
         return self.info_messages[self.status].format(*self.players)
@@ -195,26 +204,50 @@ class BattleshipsGame(object):
         self.whose_turn = (self.whose_turn + 1) % 2
 
     def make_move(self, source, text):
-        # attack there
-        pass
+        if self.players.index(source) != self.whose_turn:
+            return ["{}: It's not your turn yet!".format(source)]
+        coords_this_move = coords_to_internal(text)
+        self.next_player()
+        if coords_this_move in self.moves[self.players.index(source)]:
+            return ["{}: You already attacked that square! I guess you don't want a turn.".format(source)]
+        self.moves[self.players.index[source]].add(coords_this_move)
+        for boat in self.boats[self.whose_turn]:
+            status = boat.attack(coords_this_move)
+            if status == "dead":
+                messages = ["{}: You sunk {}'s {}.".format(source, self.players[self.whose_turn], boat.boat_type)]
+                self.boats_left_to_sink[self.players.index(source)] -= 1
+                if len(self.boats_left_to_sink[self.players.index(source)]) == 0:
+                    messages.append("{}: That was {}'s last boat. You win!"
+                                    .format(source, self.players[self.whose_turn]))
+                    index, game = battleships_bot.find_my_game(source)
+                    if not game:
+                        raise Exception("Uh, how is {} not in a game?".format(source))
+                    del battleships_bot.active_games[index]
+                    return messages
+            elif status == "hit":
+                return ["{}: You hit {}'s {}.".format(source, self.players[self.whose_turn], boat.boat_type)]
+            else:
+                return ["{}: You didn't hit anything.".format(source)]
 
     def print_moves(self, source):
-        # print the moves
-        return ['print moves!']
+        return ("{}: Your moves so far are: {}"
+                .format(source,
+                        ', '.join([coords_to_printable(coord)
+                                   for coord in self.moves[self.players.index(source)]].sort())))
 
     def set_position(self, source, text):
         boat_type, raw_location, raw_direction = text.split(' ')
         start_coords = coords_to_internal(raw_location)
         direction = self.direction_map[raw_direction]
-        new_boat = positioned_boat(boat_type, start_coords, direction)
+        new_boat = PositionedBoat(boat_type, start_coords, direction)
         player_number = self.players.index(source)
         boat_list = self.boats[player_number]
         if new_boat.off_edge_of_board(self.grid_size):
             return ["That doesn't fit there! It goes off the edge of the board."]
         for boat in boat_list:
             if new_boat.overlaps(boat):
-                return ["That overlaps with another boat you already placed:",
-                        "The {boat_type} at {coord} {direction}"
+                return ["That overlaps with another boat you already placed: ",
+                        "The {boat_type} at {coord} {direction}. You can move that boat if you want."
                         .format(boat_type=boat.boat_type,
                                 coord=coords_to_printable(boat.location),
                                 direction=self.human_direction_map[boat.direction])]
@@ -224,45 +257,66 @@ class BattleshipsGame(object):
             self.boats_to_be_positioned[player_number].remove(boat_type)
         else:
             messages.append("That one has already been placed but I'll let you move it.")
-        messages.append("Boat positioned.", "Boats still to place are: {}"
-                        .format(", ".join(self.boats_to_be_positioned)))
-        if (len(self.boats_to_be_positioned[0]) == 0 and
-            len(self.boats_to_be_positioned[1]) == 0):
-            self.status = 'playing'
-            self.bot.messenger.add_to_queue()
+        messages.append("Boat positioned.")
+        if len(self.boats_to_be_positioned[player_number]) == 0:
+            messages.append("All your boats have been placed. Game will start when your opponent has done the same.")
+            if len(self.boats_to_be_positioned[(player_number + 1) % 2]) == 0:
+                self.status = 'playing'
+                self.bot.messenger.add_to_queue(self.bot.channel,
+                                                "Game is starting between {} and {}"
+                                                .format(*self.players))
+        return messages
 
 
-class positioned_boat(object):
+boat_lengths = {
+    'carrier': 5,
+    'battleship': 4,
+    'submarine': 3,
+    'destroyer': 3,
+    'patrol': 2
+}
+
+
+class PositionedBoat(object):
     def __init__(self, boat_type, location, direction):
         self.boat_type = boat_type
         self.location = location
         self.direction = direction
-        self.boat_length = {
-            'carrier': 5,
-            'battleship': 4,
-            'submarine': 3,
-            'destroyer': 3,
-            'patrol': 2
-        }[boat_type]
+        self.boat_length = boat_lengths[boat_type]
         self.coords = [tuple([location[i] + (direction[i] * index) for i in range(2)])
                        for index in range(self.boat_length)]
+        self.coords_left_to_hit = copy(self.coords)
 
     def off_edge_of_board(self, grid_size):
         return max(self.coords[-1]) + 1 > grid_size
 
-    def overlap(self, boat):
+    def overlaps(self, boat):
         for coord in self.coords:
             if coord in boat.coords:
                 return True
         else:
             return False
 
+    def coord_hit(self, coord):
+        return coord in self.coords_left_to_hit
+
+    def attack(self, coord):
+        if self.coord_hit(coord):
+            self.coords_left_to_hit.remove(coord)
+            if len(self.coords_left_to_hit) == 0:
+                return "dead"
+            else:
+                return "hit"
+        else:
+            return "miss"
+
+
 battleships_bot = BattleshipsBot()
 
 
 def coords_to_printable(coords_internal):
-    return chr(65 + coords_internal[1]) + str(coords_internal[0])
+    return chr(65 + coords_internal[1]) + str(coords_internal[0] + 1)
 
 
 def coords_to_internal(coords_human):
-    return (ord(65 + coords_human[1]), int(coords_human[0]))
+    return (ord(65 + coords_human[1]), int(coords_human[0]) - 1)
