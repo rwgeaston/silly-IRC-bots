e78955ce880bb9e3a99d5d96bed20fa330cc79c8
rwgeaston <rwgeaston@gmail.com>
2013-09-01 16:34:21 +0100

    can accept challenges and place boats

--- bots/battleships_player_speech.py
+++ bots/battleships_player_speech.py
@@ -1,5 +1,12 @@
-from battleships_shared import boat_lengths, coords_to_internal, coords_to_printable, PositionedBoat, grid_size, direction_map, human_direction_map
+from battleships_shared import boat_lengths, coords_to_internal, coords_to_printable, PositionedBoat, \
+                               grid_size, direction_map, human_direction_map
 from random import choice, randint
+from re import match as regex_match
+from time import sleep
+
+
+battleships_bot = 'ships'
+battleships_player = BattleshipsPlayer(grid_size)
 
 
 def authorised_to_shup(source, owner):
@@ -9,6 +16,8 @@ def authorised_to_shup(source, owner):
 def what_to_say(bot, source, text, private):
     if text.startswith('{}:'.format(bot.nickname)):
         request = text[len(bot.nickname) + 1:].strip()
+    elif source == battleships_bot:
+        return battleships_player.request_for_me(bot, text)
     elif private:
         request = text
     else:
@@ -54,3 +63,19 @@ def place_boat(boat_type, grid_size):
     if orientation == (1, 0):
         coords = (coords[1], coords[0])
     return PositionedBoat(boat_type, coords, orientation)
+
+
+class BattleshipsPlayer(object):
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+
+    def request(self, bot, text):
+        challenge = regex_match('(?P<challenger>\w+) has challenged {}! Waiting for accept...'.format(target), text)
+        if challenge:
+            self.current_game = BattleshipsGame(challenge.group('challenger'))
+            bot.public("{}: accept".format(battleships_bot))
+            sleep(3)
+            bot.message(battleships_bot, generate_random_boat_positions(self.grid_size))
+            return ["\o/"]
+        else:
+            return []
